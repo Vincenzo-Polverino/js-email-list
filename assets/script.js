f5fee73d09b75dcaac968d69dfb2fa70b68e3b92
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-17 17:17:02 +0200

    add button for regen other e-mail

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,20 @@ const email_listEl = document.querySelector('.email_list')
 const buttonEl = document.querySelector('.btn')
 
 
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            const list_item = document.createElement('li');
-            list_item.textContent = response.data.response;
-            email_listEl.appendChild(list_item);
-        })
-        .catch(error => console.error('Errore:', error));
-}
+function regenerate() {
+
+    email_listEl.innerHTML = '';
 
 
-buttonEl = document.addEventListener("click", function(){
-  
+
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                const list_item = document.createElement('li');
+                list_item.textContent = response.data.response;
+                email_listEl.appendChild(list_item);
+            })
+            .catch(error => console.error('Errore:', error));
+    }
 }
-   
-  );
+buttonEl.addEventListener('click', regenerate);
